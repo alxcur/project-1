dbd487c7cba2247b426b853e80879b078b9d6e05
Michael Henderson <mhenderson557@yahoo.com>
2019-02-02 13:23:22 -0600

    Initial playlist to Dallas zipcode.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     var weatherApiKey = "5d1cc1876b69aaa35b49428d6c8f441b";
 
     // Spotify API key
-    var spotifyApiKey = "BQD1fyZmHwBfpE8X0_ISKygNSfJgXjHx5IFKLaucFnY2eARfpSCVOdZKAhGBs-lUFi32K9-0Z5K9kM8MSojxoTi6uRd8GmxeDL_P5LRFGQRyltIvUPaXM0QDjacdm-I6g_ypEblubvsUMUJBf95lJ58f-dhYXtCyMmlQPJ3odnyTJtE_rpg1NmAw2hP8GeG_a8Uy3llw0eHCsM99PQ";   
+    var spotifyApiKey = "BQBA_1VyXD49FmxW0ZfYvY2hjQ_-zO9haGyhhbSnugnpeahesd9Ydm9K-o4qzg3CXvdzwgQnc39j7i8sgxrdABlubWIb9q_fFyU0FLxFL9gGenjbhePRBY4LAyDyoaDuXNjPMqfw46d4ijm9cUPtwL8-IzBq5hHJeOWblaWPktbl_yb8GJNnUOmZf7PpOS1Zc9Fu2HH3A4UHOb1_YA";   
 
     // Default play list
     var defaultPlayList = "0e8nNKeq46thd42Z2HrXQc";
@@ -50,12 +50,15 @@ $(document).ready(function() {
                 $("#userMsg").text(city + " : " + currentWeather);
                 getPlaylistByWeather(city, currentWeather);
             },
-            error: function(data){          
+            error: function(xhr, error){
+                console.log(xhr);
+                console.log(error);          
                 $("#userMsg").text("No weather results for zipcode " + $("#inputZipCode").val().trim());    
             }  
         });
     };
 
+    // AJAX function to get the playlist based on the weather condition
     function getPlaylistByWeather(city, weatherDescription) {
         $.ajax({ 
             url: "https://api.spotify.com/v1/users/alexcurington/playlists",
@@ -89,4 +92,7 @@ $(document).ready(function() {
             }
         });
     }
+
+    $("#inputZipCode").val("75211");
+    getWeather("75211");
 });
